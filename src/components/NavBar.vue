513d37f77b95b3da3707eab3357fb1fced5324d3
igorashs <igorashsirbu@gmail.com>
2019-11-01 03:49:43 +0200

    Change font size

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
       <v-toolbar-items>
         <v-btn text>
           <v-icon left large>mdi-cloud-search</v-icon>
-          <v-toolbar-title class="display-1">
+          <v-toolbar-title class="headline">
             <span class="font-weight-bold">Weather </span>
             <span>App</span>
           </v-toolbar-title>
