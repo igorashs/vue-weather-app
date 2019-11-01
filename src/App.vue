0ac81e93d81baad1f115124d3e11112d8fdda093
igorashs <igorashsirbu@gmail.com>
2019-11-01 02:00:47 +0200

    Add nav-bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app></v-app-bar>
-    <v-content> </v-content>
+    <nav-bar />
+    <v-content></v-content>
   </v-app>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue';
+
 export default {
   name: 'App',
   data: () => ({
     //
   }),
+  components: {
+    NavBar
+  }
 };
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <v-app-bar app flat>
+      <v-toolbar-items>
+        <v-btn text>
+          <v-icon left large>mdi-cloud-search</v-icon>
+          <v-toolbar-title class="display-1">
+            <span class="font-weight-bold">Weather </span>
+            <span>App</span>
+          </v-toolbar-title>
+        </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-btn depressed fab class="grey darken-3" @click="openRepo">
+        <v-icon x-large>mdi-github-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  methods: {
+    openRepo() {
+      window.open('https://github.com/igorashs/vue-weather-app', '_blank');
+    }
+  }
+};
+</script>
