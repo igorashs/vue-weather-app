119304fbb3832bd11b4e176a69a6fc6655bb6028
igorashs <igorashsirbu@gmail.com>
2019-11-02 01:11:48 +0200

    Add celsius and fahrenheit temp

--- src/components/WeatherView.vue
+++ src/components/WeatherView.vue
@@ -1,7 +1,13 @@
 <template>
   <v-container class="d-flex justify-center" v-if="data">
     <v-card max-width="1024" min-width="320">
-      <v-card-title>Weather For {{ data.name }}</v-card-title>
+      <v-card-title class="headline">
+        <span class="font-weight-bold"> {{ data.name }}</span>
+      </v-card-title>
+      <v-card-subtitle>{{ currentDate }} {{ data.country }}</v-card-subtitle>
+      <v-card-text class="display-1"
+        >{{ fahrenheit }}<span>&deg;F</span>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
@@ -9,6 +15,21 @@
 <script>
 export default {
   name: 'WeatherView',
-  props: ['data']
+  props: ['data'],
+  computed: {
+    currentDate() {
+      return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(
+        new Date()
+      );
+    },
+    fahrenheit() {
+      const { temp } = this.data.weatherInfo;
+      return Math.round(((temp - 273.15) * 9) / 5 + 32);
+    },
+    celsius() {
+      const { temp } = this.data.weatherInfo;
+      return Math.round(temp - 273.15);
+    }
+  }
 };
 </script>
