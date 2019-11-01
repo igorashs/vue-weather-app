263ffb41273bae2f5519c4215429d81cb96d0656
igorashs <igorashsirbu@gmail.com>
2019-11-01 18:01:04 +0200

    Add weatherService

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -1,15 +1,34 @@
 <template>
   <v-container style="max-width: 512px">
-    <v-row class="align-center">
-      <v-text-field
-        class="title pa-1"
-        label="Enter Your Location Here..."
-      ></v-text-field>
-      <v-btn class="ma-3"><v-icon>mdi-magnify</v-icon></v-btn>
-    </v-row>
+    <v-form @submit.stop.prevent="findLocationData">
+      <v-row class="align-center">
+        <v-text-field
+          class="title pa-1"
+          label="Enter Your Location Here..."
+          v-model="location"
+        ></v-text-field>
+        <v-btn class="ma-3" @click="findLocationData"
+          ><v-icon>mdi-magnify</v-icon></v-btn
+        >
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 
 <script>
-export default {};
+import weatherService from '../services/weatherService';
+
+export default {
+  data() {
+    return {
+      location: '',
+      weatherData: null
+    };
+  },
+  methods: {
+    async findLocationData() {
+      this.weatherData = await weatherService.getLocationData(this.location);
+    }
+  }
+};
 </script>
---
+++ src/services/weatherService.js
@@ -0,0 +1,33 @@
+const API_KEY = '23f84a7ed3bef67a51a44d47ea5a393d';
+const API_CALL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
+export default {
+  async getLocationData(cityName) {
+    function getIcon(icon) {
+      return `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    }
+    function getDataObject(data) {
+      const { main, description, icon } = data.weather[0];
+      const { country } = data.sys;
+      const { name, main: weatherInfo } = data;
+
+      return {
+        name,
+        country,
+        main,
+        description,
+        iconUrl: getIcon(icon),
+        weatherInfo
+      };
+    }
+
+    try {
+      const res = await fetch(`${API_CALL}${cityName}&appid=${API_KEY}`);
+      const data = await res.json();
+
+      return getDataObject(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
