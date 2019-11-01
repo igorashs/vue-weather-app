14f1cbbbc9f28ec0ba80c0f8ddea54de18e7af74
igorashs <igorashsirbu@gmail.com>
2019-11-02 00:13:39 +0200

    Add weatherService

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <v-app>
     <nav-bar />
     <v-content>
-      <input-search />
+      <input-search @search-location-data="handleSearchLocationData" />
       <v-divider></v-divider>
-      <weather-view initData="weatherData" />
+      <weather-view :data="weatherData" />
     </v-content>
   </v-app>
 </template>
@@ -14,15 +14,22 @@ import NavBar from './components/NavBar.vue';
 import InputSearch from './components/InputSearch.vue';
 import WeatherView from './components/WeatherView.vue';
 
+import weatherService from './services/weatherService';
+
 export default {
   name: 'App',
   data: () => ({
-    //
+    weatherData: null
   }),
   components: {
     NavBar,
     InputSearch,
     WeatherView
+  },
+  methods: {
+    async handleSearchLocationData(location) {
+      this.weatherData = await weatherService.getLocationData(location);
+    }
   }
 };
 </script>
