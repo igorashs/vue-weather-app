3d541a6c572a96128a3e2e0c3bdf6cb492b66406
igorashs <igorashsirbu@gmail.com>
2019-11-02 00:08:43 +0200

    Revert "Add search-location-data event"
    This reverts commit e28a277febdaa068183e88c264edd0b48ac64f99.

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container style="max-width: 512px">
-    <v-form @submit.stop.prevent="onSearchLocationData">
+    <v-form @submit.stop.prevent="findLocationData">
       <v-row class="align-center">
         <v-text-field
           class="title pa-1"
           label="Enter Your Location Here..."
           v-model="location"
         ></v-text-field>
-        <v-btn class="ma-3" @click="onSearchLocationData"
+        <v-btn class="ma-3" @click="findLocationData"
           ><v-icon>mdi-magnify</v-icon></v-btn
         >
       </v-row>
@@ -16,16 +16,19 @@
 </template>
 
 <script>
+import weatherService from '../services/weatherService';
+
 export default {
   name: 'InputSearch',
   data() {
     return {
-      location: ''
+      location: '',
+      weatherData: null
     };
   },
   methods: {
-    onSearchLocationData() {
-      this.$emit('search-location-data', this.location);
+    async findLocationData() {
+      this.weatherData = await weatherService.getLocationData(this.location);
     }
   }
 };
