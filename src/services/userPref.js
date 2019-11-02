217d452158f7000aed9e9df6eb08cb302ea4436a
igorashs <igorashsirbu@gmail.com>
2019-11-02 17:13:36 +0200

    Add userPref service

---
+++ src/services/userPref.js
@@ -0,0 +1,16 @@
+export default {
+  getUserPref() {
+    if (localStorage) {
+      const userPrefData = localStorage.getItem('userPref');
+      const userPref = userPrefData ? JSON.parse(userPrefData) : {};
+      return userPref;
+    }
+  },
+  setUserPref(userPref) {
+    if (userPref && userPref.inCelsius) {
+      localStorage.setItem('userPref', JSON.stringify(userPref));
+    } else {
+      throw new Error('Invalid preferences in userPref param');
+    }
+  }
+};
