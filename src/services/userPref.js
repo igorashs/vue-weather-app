a5803c4dbbd42f36af0cd5752bf84560a49fec30
igorashs <igorashsirbu@gmail.com>
2019-11-02 18:47:37 +0200

    Fix condition

--- src/services/userPref.js
+++ src/services/userPref.js
@@ -3,11 +3,13 @@ export default {
     if (localStorage) {
       const userPrefData = localStorage.getItem('userPref');
       const userPref = userPrefData ? JSON.parse(userPrefData) : {};
+      console.log(userPref);
       return userPref;
     }
   },
   setUserPref(userPref) {
-    if (userPref && userPref.inCelsius) {
+    console.log(userPref);
+    if (userPref && userPref.hasOwnProperty('inCelsius')) {
       localStorage.setItem('userPref', JSON.stringify(userPref));
     } else {
       throw new Error('Invalid preferences in userPref param');
