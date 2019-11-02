43ec6f57fbbfca2e3a1424a3817fe9fd55c38641
igorashs <igorashsirbu@gmail.com>
2019-11-02 17:36:33 +0200

    Add NoDataDialog component

---
+++ src/components/NoDataDialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-dialog v-model="show" width="500">
+    <v-card>
+      <v-card-title class="error">
+        Something is wrong...
+      </v-card-title>
+      <v-card-text class="title pa-4">
+        Your location haven't been found!
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="$emit('toggle-dialog', false)">close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    show: Boolean
+  }
+};
+</script>
