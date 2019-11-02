fe781109f363a377889f3e05713b1cdd0d89e613
igorashs <igorashsirbu@gmail.com>
2019-11-02 18:50:20 +0200

    Remove console logs

--- src/services/userPref.js
+++ src/services/userPref.js
@@ -3,12 +3,10 @@ export default {
     if (localStorage) {
       const userPrefData = localStorage.getItem('userPref');
       const userPref = userPrefData ? JSON.parse(userPrefData) : {};
-      console.log(userPref);
       return userPref;
     }
   },
   setUserPref(userPref) {
-    console.log(userPref);
     if (userPref && userPref.hasOwnProperty('inCelsius')) {
       localStorage.setItem('userPref', JSON.stringify(userPref));
     } else {
